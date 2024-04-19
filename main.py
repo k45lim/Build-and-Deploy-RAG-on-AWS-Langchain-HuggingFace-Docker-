e218d81ac4365126ef7b9c15261b5c4b5195b0e9
k45lim <k45lim@gmail.com>
2024-04-18 20:14:24 -0600

    day 2 update

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import json
+import sys
+import boto3
+
+print("imported successfully...")
+
+prompt = """ you are a smart assistant so please let me know what is machine learning in the smartest way
+"""
+bedrock = boto3.client(service_name="bedrock-runtime")
+payload = {
+
+    "prompt": "[INST]"+prompt+"[/INST]",
+    "max_gen_len": 512,
+    "temperature": 0.3,
+    "top_p": 0.9
+}
+
+json.dump(payload)
+model_id = "meta.llama2-70b-chat-v1"
+
+response = bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body = json.loads(response, get("body").read())
+response_text = response_body["generation"]
+
+
+print(response_text)
